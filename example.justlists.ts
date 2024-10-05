ffe2f81831fb7dec8c1bfeff982cc4cbaaceddcc
Pino <130277082+PinoTRX@users.noreply.github.com>
2024-10-05 09:46:41 +0300

    Update example.justlists.ts

--- example.justlists.ts
+++ example.justlists.ts
@@ -1,132 +1,452 @@
 {
-    "name":"JustSwap Default List",
-    "tokens":[
-        {
-            "symbol":"SUN",
-            "address":"TKkeiboTkxXKJpbmVFbv4a8ov5rAfRDMf9",
-            "chainId":1,
-            "decimals":18,
-            "name":"SUN TOKEN",
-            "logoURI":"https://coin.top/production/logo/SUNLogo.178d4636.png"
-        },
-        {
-            "symbol":"BTC",
-            "address":"TN3W4H6rK2ce4vX9YnFQHwKENnHjoxb3m9",
-            "chainId":1,
-            "decimals":8,
-            "name":"Bitcoin",
-            "logoURI":"https://coin.top/production/logo/TN3W4H6rK2ce4vX9YnFQHwKENnHjoxb3m9.png"
-        },
-        {
-            "symbol":"WBTC",
-            "address":"TXpw8XeWYeTUd4quDskoUqeQPowRh4jY65",
-            "chainId":1,
-            "decimals":8,
-            "name":"Wrapped BTC",
-            "logoURI":"https://coin.top/production/logo/TXpw8XeWYeTUd4quDskoUqeQPowRh4jY65.png"
-        },
-        {
-            "symbol":"ETH",
-            "address":"THb4CqiFdwNHsWsQCs4JhzwjMWys4aqCbF",
-            "chainId":1,
-            "decimals":18,
-            "name":"Ethereum",
-            "logoURI":"https://coin.top/production/logo/THb4CqiFdwNHsWsQCs4JhzwjMWys4aqCbF.png"
-        },
-        {
-            "symbol":"WETH",
-            "address":"TXWkP3jLBqRGojUih1ShzNyDaN5Csnebok",
-            "chainId":1,
-            "decimals":18,
-            "name":"Wrapped ETH",
-            "logoURI":"https://coin.top/production/logo/TXWkP3jLBqRGojUih1ShzNyDaN5Csnebok.png"
-        },
-        {
-            "symbol":"WBTT",
-            "address":"TKfjV9RNKJJCqPvBtK8L7Knykh7DNWvnYt",
-            "chainId":1,
-            "decimals":6,
-            "name":"Wrapped BitTorrent",
-            "logoURI":"https://coin.top/production/logo/TKfjV9RNKJJCqPvBtK8L7Knykh7DNWvnYt.png"
-        },
-        {
-            "symbol":"WTRX",
-            "address":"TNUC9Qb1rRpS5CbWLmNMxXBjyFoydXjWFR",
-            "chainId":1,
-            "decimals":6,
-            "name":"Wrapped TRX",
-            "logoURI":"https://coin.top/production/upload/logo/TNUC9Qb1rRpS5CbWLmNMxXBjyFoydXjWFR.png"
-        },
-        {
-            "symbol":"JST",
-            "address":"TCFLL5dx5ZJdKnWuesXxi1VPwjLVmWZZy9",
-            "chainId":1,
-            "decimals":18,
-            "name":"JUST GOV v1.0",
-            "logoURI":"https://coin.top/production/logo/just_icon.png"
-        },
-        {
-            "symbol":"WIN",
-            "address":"TLa2f6VPqDgRE67v1736s7bJ8Ray5wYjU7",
-            "chainId":1,
-            "decimals":6,
-            "name":"WINK",
-            "logoURI":"https://coin.top/profile_images/JKtJTydD_400x400.jpg"
-        },
-        {
-            "symbol":"DICE",
-            "address":"TKttnV3FSY1iEoAwB4N52WK2DxdV94KpSd",
-            "chainId":1,
-            "decimals":6,
-            "name":"TRONbetDice",
-            "logoURI":"https://coin.top/production/logo/TKttnV3FSY1iEoAwB4N52WK2DxdV94KpSd.jpg"
-        },
-        {
-            "symbol":"LIVE",
-            "address":"TVgAYofpQku5G4zenXnvxhbZxpzzrk8WVK",
-            "chainId":1,
-            "decimals":6,
-            "name":"TRONbetLive",
-            "logoURI":"https://coin.top/production/upload/logo/TVgAYofpQku5G4zenXnvxhbZxpzzrk8WVK.png"
-        },
-        {
-            "symbol":"USDT",
-            "address":"TR7NHqjeKQxGTCi8q8ZY4pL8otSzgjLj6t",
-            "chainId":1,
-            "decimals":6,
-            "name":"Tether USD",
-            "logoURI":"https://coin.top/production/logo/usdtlogo.png"
-        },
-        {
-            "symbol":"USDJ",
-            "address":"TMwFHYXLJaRUPeW6421aqXL4ZEzPRFGkGT",
-            "chainId":1,
-            "decimals":18,
-            "name":"JUST Stablecoin v1.0",
-            "logoURI":"https://coin.top/production/logo/usdj.png"
-        },
-        {
-            "symbol":"LTC",
-            "address":"TR3DLthpnDdCGabhVDbD3VMsiJoCXY3bZd",
-            "chainId":1,
-            "decimals":8,
-            "name":"Litecoin",
-            "logoURI":"https://coin.top/production/logo/TR3DLthpnDdCGabhVDbD3VMsiJoCXY3bZd.png"
-        },
-        {
-            "symbol":"HT",
-            "address":"TDyvndWuvX5xTBwHPYJi7J3Yq8pq8yh62h",
-            "chainId":1,
-            "decimals":18,
-            "name":"HuobiToken",
-            "logoURI":"https://coin.top/production/logo/TDyvndWuvX5xTBwHPYJi7J3Yq8pq8yh62h.png"
-        }
-    ],
-    "logoURI":"https://justswap.io/favicon.ico",
-    "version":{
-        "patch":1,
-        "major":1,
-        "minor":0
+  "name": "SunSwap Default List",
+  "tokens": [
+    {
+      "symbol": "SUN",
+      "address": "TSSMHYeV2uE9qYH95DqyoCuNCzEL1NvU3S",
+      "chainId": 1,
+      "decimals": 18,
+      "name": "SUN",
+      "logoURI": "https://static.tronscan.org/production/logo/TSSMHYeV2uE9qYH95DqyoCuNCzEL1NvU3S.png"
     },
-    "timestamp":1611147166000
+    {
+      "symbol": "BTT",
+      "address": "TAFjULxiVgT4qWk6UZwjqwZXTSaGaqnVp4",
+      "chainId": 1,
+      "decimals": 18,
+      "name": "BitTorrent",
+      "logoURI": "https://static.tronscan.org/production/logo/1002000.png"
+    },
+    {
+      "symbol": "SUNOLD",
+      "address": "TKkeiboTkxXKJpbmVFbv4a8ov5rAfRDMf9",
+      "chainId": 1,
+      "decimals": 18,
+      "name": "SUNOLD",
+      "logoURI": "https://static.tronscan.org/production/logo/SUNLogo.178d4636.png"
+    },
+    {
+      "symbol": "NFT",
+      "address": "TFczxzPhnThNSqr5by8tvxsdCFRRz6cPNq",
+      "chainId": 1,
+      "decimals": 6,
+      "name": "APENFT",
+      "logoURI": "https://static.tronscan.org/production/upload/logo/TFczxzPhnThNSqr5by8tvxsdCFRRz6cPNq.png"
+    },
+    {
+      "symbol": "BTC",
+      "address": "TN3W4H6rK2ce4vX9YnFQHwKENnHjoxb3m9",
+      "chainId": 1,
+      "decimals": 8,
+      "name": "Bitcoin",
+      "logoURI": "https://static.tronscan.org/production/logo/TN3W4H6rK2ce4vX9YnFQHwKENnHjoxb3m9.png"
+    },
+    {
+      "symbol": "WBTC",
+      "address": "TXpw8XeWYeTUd4quDskoUqeQPowRh4jY65",
+      "chainId": 1,
+      "decimals": 8,
+      "name": "Wrapped BTC",
+      "logoURI": "https://static.tronscan.org/production/logo/TXpw8XeWYeTUd4quDskoUqeQPowRh4jY65.png"
+    },
+    {
+      "symbol": "WETH",
+      "address": "TXWkP3jLBqRGojUih1ShzNyDaN5Csnebok",
+      "chainId": 1,
+      "decimals": 18,
+      "name": "Wrapped ETH",
+      "logoURI": "https://static.tronscan.org/production/logo/TXWkP3jLBqRGojUih1ShzNyDaN5Csnebok.png"
+    },
+    {
+      "symbol": "WBTT",
+      "address": "TKfjV9RNKJJCqPvBtK8L7Knykh7DNWvnYt",
+      "chainId": 1,
+      "decimals": 6,
+      "name": "Wrapped BitTorrent",
+      "logoURI": "https://static.tronscan.org/production/logo/TKfjV9RNKJJCqPvBtK8L7Knykh7DNWvnYt.png"
+    },
+    {
+      "symbol": "WTRX",
+      "address": "TNUC9Qb1rRpS5CbWLmNMxXBjyFoydXjWFR",
+      "chainId": 1,
+      "decimals": 6,
+      "name": "Wrapped TRX",
+      "logoURI": "https://static.tronscan.org/production/upload/logo/TNUC9Qb1rRpS5CbWLmNMxXBjyFoydXjWFR.png"
+    },
+    {
+      "symbol": "JST",
+      "address": "TCFLL5dx5ZJdKnWuesXxi1VPwjLVmWZZy9",
+      "chainId": 1,
+      "decimals": 18,
+      "name": "JUST GOV v1.0",
+      "logoURI": "https://static.tronscan.org/production/logo/just_icon.png"
+    },
+    {
+      "symbol": "WIN",
+      "address": "TLa2f6VPqDgRE67v1736s7bJ8Ray5wYjU7",
+      "chainId": 1,
+      "decimals": 6,
+      "name": "WINK",
+      "logoURI": "https://static.tronscan.org/profile_images/JKtJTydD_400x400.jpg"
+    },
+    {
+      "symbol": "USDT",
+      "address": "TR7NHqjeKQxGTCi8q8ZY4pL8otSzgjLj6t",
+      "chainId": 1,
+      "decimals": 6,
+      "name": "Tether USD",
+      "logoURI": "https://static.tronscan.org/production/logo/usdtlogo.png"
+    },
+    {
+      "symbol": "USDJ",
+      "address": "TMwFHYXLJaRUPeW6421aqXL4ZEzPRFGkGT",
+      "chainId": 1,
+      "decimals": 18,
+      "name": "JUST Stablecoin v1.0",
+      "logoURI": "https://static.tronscan.org/production/logo/usdj.png"
+    },
+    {
+      "symbol": "TUSD",
+      "address": "TUpMhErZL2fhh4sVNULAbNKLokS4GjC1F4",
+      "chainId": 1,
+      "decimals": 18,
+      "name": "TrueUSD",
+      "logoURI": "https://static.tronscan.org/production/logo/TUpMhErZL2fhh4sVNULAbNKLokS4GjC1F4.png"
+    },
+    {
+      "symbol": "LTC",
+      "address": "TR3DLthpnDdCGabhVDbD3VMsiJoCXY3bZd",
+      "chainId": 1,
+      "decimals": 8,
+      "name": "Litecoin",
+      "logoURI": "https://static.tronscan.org/production/logo/TR3DLthpnDdCGabhVDbD3VMsiJoCXY3bZd.png"
+    },
+    {
+      "symbol": "HT",
+      "address": "TDyvndWuvX5xTBwHPYJi7J3Yq8pq8yh62h",
+      "chainId": 1,
+      "decimals": 18,
+      "name": "HuobiToken",
+      "logoURI": "https://static.tronscan.org/production/logo/TDyvndWuvX5xTBwHPYJi7J3Yq8pq8yh62h.png"
+    },
+    {
+      "symbol": "USDD",
+      "address": "TPYmHEhy5n8TCEfYGqW2rPxsghSfzghPDn",
+      "chainId": 1,
+      "decimals": 18,
+      "name": "Decentralized USD",
+      "logoURI": "https://static.tronscan.org/production/upload/logo/TPYmHEhy5n8TCEfYGqW2rPxsghSfzghPDn.png"
+    },
+    {
+      "symbol": "sTRX",
+      "address": "TU3kjFuhtEo42tsCBtfYUAZxoqQ4yuSLQ5",
+      "chainId": 1,
+      "decimals": 18,
+      "name": "staked TRX",
+      "logoURI": "https://static.tronscan.org/production/upload/logo/new/TU3kjFuhtEo42tsCBtfYUAZxoqQ4yuSLQ5.png"
+    },
+    {
+      "symbol": "stUSDT",
+      "address": "TThzxNRLrW2Brp9DcTQU8i4Wd9udCWEdZ3",
+      "chainId": 1,
+      "decimals": 18,
+      "name": "Staked USDT",
+      "logoURI": "https://static.tronscan.org/production/upload/logo/new/stUSDT_logo.png"
+    },
+    {
+      "symbol": "HTX",
+      "address": "TUPM7K8REVzD2UdV4R5fe5M8XbnR2DdoJ6",
+      "chainId": 1,
+      "decimals": 18,
+      "name": "HTX",
+      "logoURI": "https://static.tronscan.org/production/upload/logo/new/HTX.png"
+    },
+    {
+      "symbol": "SUNDOG",
+      "address": "TXL6rJbvmjD46zeN1JssfgxvSo99qC8MRT",
+      "chainId": 1,
+      "decimals": 18,
+      "name": "Sundog",
+      "logoURI": "https://cdn.sunpump.meme/public/logo/SUNDOG_TXr7if_EzxfYukzq9ZU.png"
+    },
+    {
+      "symbol": "FOFAR",
+      "address": "TUFonyWZ4Tza5MzgDj6g2u5rfdGoRVYG7g",
+      "chainId": 1,
+      "decimals": 18,
+      "name": "FOFAR",
+      "logoURI": "https://cdn.sunpump.meme/public/logo/FOFAR_THQg9C_IavjUsY3Wun7.jpg"
+    },
+    {
+      "symbol": "IVfun",
+      "address": "TSig7sWzEL2K83mkJMQtbyPpiVSbR6pZnb",
+      "chainId": 1,
+      "decimals": 18,
+      "name": "Invest Zone",
+      "logoURI": "https://cdn.sunpump.meme/public/logo/IVfun_TYE2hX_1rR7Q8GfwFKa.jpg"
+    },
+    {
+      "symbol": "MUNCAT",
+      "address": "TE2T2vLnEQT1XW647EAQAHWqd6NZL1hweR",
+      "chainId": 1,
+      "decimals": 18,
+      "name": "MUNCAT",
+      "logoURI": "https://cdn.sunpump.meme/public/logo/MUNCAT_TJ44MB_FKFM8Y8eyhH2.jpeg"
+    },
+    {
+      "symbol": "SUNWUKONG",
+      "address": "TP3prcvQknVthrVnn281cKST56eWiLgJJM",
+      "chainId": 1,
+      "decimals": 18,
+      "name": "SunWukong",
+      "logoURI": "https://cdn.sunpump.meme/public/logo/SUNWUKONG_TCyGhb_xSV6IxtwuU33.png"
+    },
+    {
+      "symbol": "CZ",
+      "address": "TSZu2myGg42ZEsT7RvvbVvhVS3kf9f1e57",
+      "chainId": 1,
+      "decimals": 18,
+      "name": "Changpeng Zhao",
+      "logoURI": "https://cdn.sunpump.meme/public/logo/CZ_TBDSE9_iEzd2xG48TpN.jpeg"
+    },
+    {
+      "symbol": "SUNCAT",
+      "address": "TAwAg9wtQzTMFsijnSFotJrpxhMm3AqW1d",
+      "chainId": 1,
+      "decimals": 18,
+      "name": "SUNCAT",
+      "logoURI": "https://cdn.sunpump.meme/public/logo/SUNCAT_TWj3Mp_ynpQjPlfRZu9.jpeg"
+    },
+    {
+      "symbol": "Tcat",
+      "address": "TVgHqeP41s3qMDH3oKBsScEUzvyXw6bKAm",
+      "chainId": 1,
+      "decimals": 18,
+      "name": "Tron Cat",
+      "logoURI": "https://cdn.sunpump.meme/public/logo/Tcat_TXv3PZ_f4HN7mpJqQ9V.png"
+    },
+    {
+      "symbol": "BULL",
+      "address": "TAt4ufXFaHZAEV44ev7onThjTnF61SEaEM",
+      "chainId": 1,
+      "decimals": 18,
+      "name": "Tron Bull",
+      "logoURI": "https://cdn.sunpump.meme/public/logo/BULL_TYvwPp_negdGNxhFCpp.png"
+    },
+    {
+      "symbol": "SUNDOGE",
+      "address": "TAz6oGWhsmHPp7Ap6khmAYxjfHFYokqdQ4",
+      "chainId": 1,
+      "decimals": 18,
+      "name": "SUNDOGE",
+      "logoURI": "https://cdn.sunpump.meme/public/logo/SUNDOGE_THL9Hf_XQ7Lq2GubqOE.PNG"
+    },
+    {
+      "symbol": "ROCK",
+      "address": "TJvwMR3RjHc8jA9QwwjWGANrR3Y4scLSZm",
+      "chainId": 1,
+      "decimals": 18,
+      "name": "ROCK",
+      "logoURI": "https://cdn.sunpump.meme/public/logo/ROCK_TK157u_SW8560kjmR1W.jpeg"
+    },
+    {
+      "symbol": "TDOG",
+      "address": "TAPxMfHHgFyZMHuuz8wL56pi54eB2xkK2q",
+      "chainId": 1,
+      "decimals": 18,
+      "name": "Tron Dog",
+      "logoURI": "https://cdn.sunpump.meme/public/logo/TDOG_TW2UbF_SHNKw5HVdKEF.PNG"
+    },
+    {
+      "symbol": "PSYOP",
+      "address": "TBFr8v7HkKGydiHAjDL8NzqYf7cJqNsTVY",
+      "chainId": 1,
+      "decimals": 18,
+      "name": "PSYOPTRON",
+      "logoURI": "https://cdn.sunpump.meme/public/logo/PSYOP_TEX3q5_JlNNNeGizAHQ.png"
+    },
+    {
+      "symbol": "TRONKEY",
+      "address": "TRHsKfoPJxFHnJ4wJ8Zc9nmSNAyaNYqff7",
+      "chainId": 1,
+      "decimals": 18,
+      "name": "TRONKEY",
+      "logoURI": "https://cdn.sunpump.meme/public/logo/TRONKEY_TVFkWK_9m3fDI3ioDiW.jpeg"
+    },
+    {
+      "symbol": "HOTSUN",
+      "address": "TCXTVfR7pFH8voLyqhbUGXdiYaBkTDmYCQ",
+      "chainId": 1,
+      "decimals": 18,
+      "name": "HOTSUN",
+      "logoURI": "https://cdn.sunpump.meme/public/logo/HOTSUN_TUueYg_emDoYuKXklz4.jpeg"
+    },
+    {
+      "symbol": "SUNKEY",
+      "address": "TJgB8bFWzMQfbqmCicf39XHby5uB5qoEQq",
+      "chainId": 1,
+      "decimals": 18,
+      "name": "SUNKEY",
+      "logoURI": "https://cdn.sunpump.meme/public/logo/SUNKEY_TArygL_CL1PcUh6ZQr5.jpg"
+    },
+    {
+      "symbol": "CZC",
+      "address": "TRJBN2ninnLKUUDR1f686goCYetPcPed8f",
+      "chainId": 1,
+      "decimals": 18,
+      "name": "Crypto Zillion Club",
+      "logoURI": "https://cdn.sunpump.meme/public/logo/CZC_TH5Eyn_1tR9tsfvqbYq.png"
+    },
+    {
+      "symbol": "TBEER",
+      "address": "TKCJp1q9325BfQbu2Suh7mshQs8ijuqv3y",
+      "chainId": 1,
+      "decimals": 18,
+      "name": "TRON BEER",
+      "logoURI": "https://cdn.sunpump.meme/public/logo/TBEER_TXMN3d_FSZeVq8A5nzi.jpg"
+    },
+    {
+      "symbol": "VIKITA",
+      "address": "TP7r1pDoS1snMjEJE1kE17GRt3Df4mYuZz",
+      "chainId": 1,
+      "decimals": 18,
+      "name": "VIKITA",
+      "logoURI": "https://cdn.sunpump.meme/public/logo/VIKITA_TVNpDy_QRP17X2aRh0E.jpg"
+    },
+    {
+      "symbol": "MEOWKA",
+      "address": "TYpSKB5hfaKpRDwS2p7bait3xuCKYnwDKJ",
+      "chainId": 1,
+      "decimals": 18,
+      "name": "Meowka Neko",
+      "logoURI": "https://cdn.sunpump.meme/public/logo/MEOWKA_TFD7X8_2AtEA0p50jde.PNG"
+    },
+    {
+      "symbol": "TWX",
+      "address": "TTFreuJ4pYDaCeEMEtiR1GQDwPPrS4jKFk",
+      "chainId": 1,
+      "decimals": 18,
+      "name": "Twiskers",
+      "logoURI": "https://cdn.sunpump.meme/public/logo/TWX_TF8z6B_bQu1A9s9Wox2.png"
+    },
+    {
+      "symbol": "PUSS",
+      "address": "TX5eXdf8458bZ77fk8xdvUgiQmC3L93iv7",
+      "chainId": 1,
+      "decimals": 18,
+      "name": "PUSS",
+      "logoURI": "https://cdn.sunpump.meme/public/logo/PUSS_TQRxQN_ls9y5lDjLoeb.png"
+    },
+    {
+      "symbol": "$Afro",
+      "address": "TDnXXUXH37zEojEfrvYziS6yKSpYmkdjHE",
+      "chainId": 1,
+      "decimals": 18,
+      "name": "Afro",
+      "logoURI": "https://cdn.sunpump.meme/public/logo/$Afro_TPeQ55_cHUMaVSFmoe8.png"
+    },
+    {
+      "symbol": "CDOG",
+      "address": "TVXmroHbJsJ6rVm3wGn2G9723yz3Kbqp9x",
+      "chainId": 1,
+      "decimals": 18,
+      "name": "Cyber Dog",
+      "logoURI": "https://cdn.sunpump.meme/public/logo/CDOG_TQU2Lr_ZBlOpcEb0sXM.jpg"
+    },
+    {
+      "symbol": "TBULL",
+      "address": "TPeoxx1VhUMnAUyjwWfximDYFDQaxNQQ45",
+      "chainId": 1,
+      "decimals": 18,
+      "name": "Tron Bull",
+      "logoURI": "https://cdn.sunpump.meme/public/logo/TBULL_TSfV1B_YQaduaEhm0cB.jpg"
+    },
+    {
+      "symbol": "USDCOLD",
+      "address": "TEkxiTehnzSmSe2XqrBj4w32RUN966rdz8",
+      "chainId": 1,
+      "decimals": 6,
+      "name": "USD Coin Old",
+      "logoURI": "https://static.tronscan.org/production/upload/logo/TEkxiTehnzSmSe2XqrBj4w32RUN966rdz81.png"
+    },
+    {
+      "symbol": "MOONDOG",
+      "address": "TMWD9A3N3EhhEhiTCkEESBrVTosNmMJy8T",
+      "chainId": 1,
+      "decimals": 18,
+      "name": "MOONDOG",
+      "logoURI": "https://cdn.sunpump.meme/public/logo/MOONDOG_TQXe52_BoDeNysxsme5.png"
+    },
+    {
+      "symbol": "SunJoker",
+      "address": "TRGEYcmBSAz3PswhtAHcUPjGbuGr1H9Fza",
+      "chainId": 1,
+      "decimals": 18,
+      "name": "SunJoker",
+      "logoURI": "https://cdn.sunpump.meme/public/logo/SunJoker_TM2wL8_WsJV6iyQA2Fr.jpg"
+    },
+    {
+      "symbol": "ETHB",
+      "address": "TRFe3hT5oYhjSZ6f3ji5FJ7YCfrkWnHRvh",
+      "chainId": 1,
+      "decimals": 18,
+      "name": "Ethereum BTTC-Bridged",
+      "logoURI": "https://static.tronscan.org/production/logo/TRFe3hT5oYhjSZ6f3ji5FJ7YCfrkWnHRvh.png"
+    },
+    {
+      "symbol": "ETH",
+      "address": "THb4CqiFdwNHsWsQCs4JhzwjMWys4aqCbF",
+      "chainId": 1,
+      "decimals": 18,
+      "name": "Ethereum",
+      "logoURI": "https://static.tronscan.org/production/logo/THb4CqiFdwNHsWsQCs4JhzwjMWys4aqCbF.png"
+    },
+    {
+      "symbol": "LFG",
+      "address": "TWLutE1DE5S7TBT8hbkjLtdtevBK5EjhQF",
+      "chainId": 1,
+      "decimals": 18,
+      "name": "Gold Funky Llama",
+      "logoURI": "https://cdn.sunpump.meme/public/logo/LFG_TWp48S_ZwwJaOSftrfh.gif"
+    },
+    {
+      "symbol": "RICE",
+      "address": "TAcN65mudDue4bqe7LTmHgjiiQdT92H1we",
+      "chainId": 1,
+      "decimals": 18,
+      "name": "Rice",
+      "logoURI": "https://cdn.sunpump.meme/public/logo/RICE_TSi28N_wkWWvwXgzh7D.jpg"
+    },
+    {
+      "symbol": "LABR",
+      "address": "TMEvVHCUngZ6JfuvnH74cX8UFw1KedAuhR",
+      "chainId": 1,
+      "decimals": 18,
+      "name": "Labrador",
+      "logoURI": "https://cdn.sunpump.meme/public/logo/LABR_TTGSZE_HG9I7qi3BY11.png"
+    },
+    {
+      "symbol": "ZAPO",
+      "address": "TPUZseUsV89tM157aLfkuKYEaRKzBirmRb",
+      "chainId": 1,
+      "decimals": 18,
+      "name": "ZAPO",
+      "logoURI": "https://cdn.sunpump.meme/public/logo/ZAPO_TTFoKx_7z8qYzh3dinV.png"
+    },
+    {
+      "symbol": "«Pino»",
+      "address": "TAbkLGngV1k8bNb5hnqW7oX46FG4Dbs5e2",
+      "chainId": 1,
+      "decimals": 18,
+      "name": "«Pino the Dino»",
+      "logoURI": "https://cdn.sunpump.meme/public/logo/Pino_TRneu6_FMMdCbfHTxOi.PNG"
+    }
+  ],
+  "logoURI": "https://sun.io/favicon.ico",
+  "version": {
+    "patch": 0,
+    "major": 1,
+    "minor": 43
+  },
+  "timestamp": 1727697600000
 }
